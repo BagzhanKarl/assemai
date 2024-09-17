817500f1324b078a8cca0563b652f3ebee727c82
newri <bagzhankarl@gmail.com>
2024-09-17 16:14:28 +0600

    Start hook

--- whatsapp/ai/index.php
+++ whatsapp/ai/index.php
@@ -1,5 +1,5 @@
 <?php
 $webhookData = file_get_contents('php://input');
 $data = json_decode($webhookData, true);
-file_put_contents('webhook.log', print_r($data, true));
+file_put_contents('webhook.log', print_r($_POST, true));
 ?>
