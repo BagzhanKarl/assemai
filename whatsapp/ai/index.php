a2ee3bff8864b1facf4c417868e5859f24f53ffd
newri <bagzhankarl@gmail.com>
2024-09-17 16:13:09 +0600

    Start hook

--- whatsapp/ai/index.php
+++ whatsapp/ai/index.php
@@ -1,11 +1,5 @@
 <?php
-require '../../db.php'; // Подключение к базе данных
-
-// Получаем данные из вебхука
 $webhookData = file_get_contents('php://input');
 $data = json_decode($webhookData, true);
-$hook = R::dispense('hooks');
-$hook->web = $webhookData;
-$hook->data = $data;
-R::store($hook);
+file_put_contents('webhook.log', print_r($data, true));
 ?>
