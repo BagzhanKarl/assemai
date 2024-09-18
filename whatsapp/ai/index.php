296d74301e50c28d5bd47e8c54846270a0b49a45
newri <bagzhankarl@gmail.com>
2024-09-18 17:22:59 +0600

    Start hook

--- whatsapp/ai/index.php
+++ whatsapp/ai/index.php
@@ -6,12 +6,16 @@ require '../../db.php';
 $webhookData = file_get_contents('php://input');
 $data = json_decode($webhookData, true);
 
-// Логируем данные для проверки
-file_put_contents('webhook.log', print_r($data, true));
-
+$messages = $data['messages'];
 // Сохраняем данные в базу как есть
 $msg = R::dispense('webhooks');
-$msg->data = $webhookData;
+$msg->messageid = $messages['id'];
+$msg->from_me = $messages['from_me'];
+$msg->type = $messages['type'];
+$msg->chat_id = $messages['chat_id'];
+$msg->phone = $messages['phone'];
+$msg->text = $messages['text']['body'];
+$msg->timestamp = $messages['timestamp'];
 R::store($msg);
 
 echo 'Данные успешно сохранены!';
--- whatsapp/index.php
+++ whatsapp/index.php
@@ -2,3 +2,8 @@
 
 $url = 'https://gate.whapi.cloud/';
 $token = 'rmXQbivTFcJHwIpQtDWu58co0TaeqOCN';
+
+$data = '{"messages":[{"id":"OoKFxugCJB.eOA-gOkSGu1bag","from_me":false,"type":"text","chat_id":"77761174378@s.whatsapp.net","timestamp":1726658279,"source":"mobile","text":{"body":"Добрый день!"},"from":"77761174378","from_name":"3D"}],"event":{"type":"messages","event":"post"},"channel_id":"DAREDL-B3F92"}';
+
+$data = json_decode($data, true);
+$messages = $data['messages'];
