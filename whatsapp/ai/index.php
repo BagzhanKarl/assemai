78ceb8df20087b0d9a2bf3bfddcc3df575604483
newri <bagzhankarl@gmail.com>
2024-09-19 12:47:36 +0600

    Start hook

--- whatsapp/ai/index.php
+++ whatsapp/ai/index.php
@@ -2,36 +2,22 @@
 // Подключаемся к базе данных
 require '../../db.php';
 
-// Получаем данные из вебхука
 $webhookData = file_get_contents('php://input');
 $data = json_decode($webhookData, true);
 
-// Проверяем, что данные корректны
-if (isset($data['messages'][0])) {
-    // Извлекаем данные из массива
-    $message = $data['messages'][0];
-    $event = $data['event'];
+$message = $data['messages'][0];
+$msg = R::dispense('webhooks');
+$msg->messagid = $message['id'];
+$msg->fromme = $message['from_me'];
+$msg->messagetype = $message['type'];
+$msg->chatid = $message['chat_id'];
+$msg->timestamp = $message['timestamp'];
+$msg->source = $message['source'];
+$msg->body = $message['text']['body'];
+$msg->number = $message['from'];
+$msg->name = $message['from_name'];
+$msg->channelid = $data['channel_id'];
 
-    // Подготавливаем объект для сохранения
-    $msg = R::dispense('webhooks');
-    $msg->message_id = $message['id'];
-    $msg->from_me = $message['from_me'];
-    $msg->message_type = $message['type'];
-    $msg->chat_id = $message['chat_id'];
-    $msg->timestamp = $message['timestamp'];
-    $msg->source = $message['source'];
-    $msg->body = $message['text']['body'];
-    $msg->from_number = $message['from'];
-    $msg->from_name = $message['from_name'];
-    $msg->event_type = $event['type'];
-    $msg->event_event = $event['event'];
-    $msg->channel_id = $data['channel_id'];
-
-    // Сохраняем данные в базу
-    R::store($msg);
-
-    echo 'Данные успешно сохранены!';
-} else {
-    echo 'Некорректные данные';
-}
+// Сохраняем данные в базу
+R::store($msg);
 ?>
